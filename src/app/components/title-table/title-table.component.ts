867d8f9705a8f25dbde53f7578d40f651e10a3fd
squwsun <squwsun@gmail.com>
2024-11-26 14:26:55 -0500

    adding search title functionality

--- src/app/components/title-table/title-table.component.ts
+++ src/app/components/title-table/title-table.component.ts
@@ -8,6 +8,9 @@ import { MatTableModule } from '@angular/material/table';
 import { RouterModule } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteTitleDialogComponent } from './delete-title-dialog/delete-title-dialog.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-title-table',
@@ -18,7 +21,10 @@ import { DeleteTitleDialogComponent } from './delete-title-dialog/delete-title-d
     MatIconModule, 
     MatToolbarModule, 
     MatTableModule, 
-    RouterModule
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
   templateUrl: './title-table.component.html',
   styleUrl: './title-table.component.css'
@@ -27,6 +33,8 @@ export class TitleTableComponent implements OnInit {
   readonly http = inject(HttpClient)
   readonly dialog = inject(MatDialog);
   titlesList: BookTitle[] = []
+  filteredTitlesList: BookTitle[] = []
+  searchTerm: string = '';
 
   ngOnInit(): void {
     this.getAllTitles()
@@ -35,7 +43,18 @@ export class TitleTableComponent implements OnInit {
   getAllTitles(): void {
     this.http.get<TitleResponseModel>('title/table-display').subscribe((res: TitleResponseModel) => {
       this.titlesList = res.Data
+      this.filteredTitlesList = [...this.titlesList]
     })
+  }
+
+  applyFilter(): void {
+    if (!this.searchTerm) {
+      this.filteredTitlesList = [...this.titlesList]
+      return;
+    }
+    this.filteredTitlesList = this.titlesList.filter(title =>
+      title.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
   }
 
   deleteTitle(title_info: BookTitle): void {
